9f858830e26f99b1e01a0f570fee9058d331402f
itisMavis <maiss.a.khalaf704@gmail.com>
2026-04-01 12:55:31 +0300

    TODO 1

---
+++ utils/operations.js
@@ -0,0 +1,14 @@
+export function add(numbers) {
+    return {}
+    }
+    export function subtract(numbers) {
+      return {}
+    }
+
+    export function multiply(numbers) {
+      return {}
+    }
+
+    export function divide(numbers) {
+      return {}
+    }
---
+++ utils/parser.js
@@ -0,0 +1,9 @@
+import _ from "lodash";
+
+export function parseNumbers(input) {
+    return {}
+}
+
+export function isValidOperation(operation) {
+    return {}
+}
